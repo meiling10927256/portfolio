df3186cea353144f916d56c1f1f701677cfd2998
mei <92775573+meiling10927256@users.noreply.github.com>
2023-10-03 19:20:49 +0800

    Delete Pipeline CPU directory

--- Pipeline CPU/mux2.v
+++
@@ -1,9 +0,0 @@
-module mux2( sel, a, b, y );
-parameter bitwidth = 32; 
-input sel ;
-input [bitwidth-1 : 0] a, b ; // a:別人的 b:自己的
-output [bitwidth-1 : 0] y ;
- 
-  assign y = ( sel ) ? b : a ;
-  
-endmodule
